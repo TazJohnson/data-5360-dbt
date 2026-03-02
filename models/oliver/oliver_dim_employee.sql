3e2d51d3b449ba83efaa405ff4cec4272bf4087f
Taz Johnson <A02403527@usu.edu>
2026-03-02 18:46:38 +0000

    Finished Customer and Employee

---
+++ models/oliver/oliver_dim_employee.sql
@@ -0,0 +1,17 @@
+{{ config(
+    materialized = 'table',
+    schema = 'dw_oliver'
+    )
+}}
+
+select
+employee_id as employee_key,
+employee_id,
+email,
+position,
+hire_date,
+first_name,
+last_name,
+phone_number,
+state
+FROM {{ source('oliver_landing', 'employee') }}
