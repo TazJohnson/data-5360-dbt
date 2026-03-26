a6875690708ad5f81770c81df53e8facac4eb52d
Taz Johnson <A02403527@usu.edu>
2026-03-26 18:25:10 +0000

    Finished Project Deliverable Part 2.

---
+++ models/EcoEssentials/ecoessentials_dim_customer.sql
@@ -0,0 +1,18 @@
+{{ config(
+    materialized = 'table',
+    schema = 'dw_ecoessentials'
+    )
+}}
+
+select
+customer_id as customer_key,
+customer_id,
+customer_first_name,
+customer_last_name,
+customer_phone,
+customer_address,
+customer_city,
+customer_state,
+customer_zip,
+customer_country
+FROM {{ source('ecoessentials_landing', 'customer') }}
